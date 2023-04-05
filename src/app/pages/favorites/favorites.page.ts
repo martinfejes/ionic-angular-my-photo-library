4d07092d25faf5423727a4dd448dd1d2b2ce4200
Martin Fejes <Martin_Fejes1@epam.com>
2023-04-05 18:43:00 +0200

    STEP-07: Implement favorites page logic

--- src/app/pages/favorites/favorites.page.ts
+++ src/app/pages/favorites/favorites.page.ts
@@ -2,16 +2,46 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Photo } from '@models/photo';
+import { FavoritesService } from '@services/favorites.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PageHeaderComponent, PhotoCardComponent } from '@components';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.page.html',
   styleUrls: ['./favorites.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    PhotoCardComponent,
+    PageHeaderComponent,
+  ],
 })
 export class FavoritesPage implements OnInit {
-  constructor() {}
+  photos!: Photo[];
 
-  ngOnInit() {}
+  constructor(
+    private favoritesService: FavoritesService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((_) => {
+      this.photos = [];
+      this.getPhotos();
+    });
+  }
+
+  getPhotos(): void {
+    const photos = this.favoritesService.getAll();
+    this.photos = photos;
+  }
+
+  openDetail(photo: Photo): void {
+    this.router.navigateByUrl(`/detail/${photo.id}`);
+  }
 }
