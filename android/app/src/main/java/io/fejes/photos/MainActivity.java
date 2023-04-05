f06d2cbd7dfd4ecff16d07881d82f02cf6997d71
Martin Fejes <Martin_Fejes1@epam.com>
2023-04-05 11:48:42 +0200

    STEP-02: Generate Android and iOS folders, add app icon and splash screen

---
+++ android/app/src/main/java/io/fejes/photos/MainActivity.java
@@ -0,0 +1,5 @@
+package io.fejes.photos;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'my-photo-library3',
+  appId: 'io.fejes.photos',
+  appName: 'my-photo-library',
   webDir: 'www',
   bundledWebRuntime: false
 };
